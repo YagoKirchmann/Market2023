9d0d22c52ecb3b2c95fa13bc3745719347f5ec14
Your Name <you@example.com>
2023-06-13 21:13:51 -0300

    finalizando DAO

--- src/main/java/com/mycompany/newmaketmaven/services/CondPgtoService.java
+++ src/main/java/com/mycompany/newmaketmaven/services/CondPgtoService.java
@@ -8,27 +8,27 @@ import com.mycompany.newmaketmaven.modelDAO.CondPgtoDAO;
 public class CondPgtoService{
 
     
-    public static void criar(CondPgto objeto) {
+    public static void criar(CondicaoPgto objeto) {
         CondPgtoDAO.getInstance().create(objeto);
     }
     
-    public static CondPgto buscar(int codigo) {
+    public static CondicaoPgto buscar(int codigo) {
         return CondPgtoDAO.getInstance().retrieve(codigo);
     }
 
-    public static CondPgto buscar(String descricao) {
+    public static CondicaoPgto buscar(String descricao) {
        return CondPgtoDAO.getInstance().retrieve(descricao);
     }
     
-    public static List<CondPgto> buscar() {
+    public static List<CondicaoPgto> buscar() {
        return  CondPgtoDAO.getInstance().retrieve();
     }
     
-    public static void atualizar(CondPgto objeto) {
+    public static void atualizar(CondicaoPgto objeto) {
       CondPgtoDAO.getInstance().update(objeto);
     }
   
-    public static void excluir(CondPgto objeto) {
+    public static void excluir(CondicaoPgto objeto) {
         CondPgtoDAO.getInstance().delete(objeto);
     }
    
